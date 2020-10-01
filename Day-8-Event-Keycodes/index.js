b51cb98dd4b28b2c5fb4f46d17b565e14b848e2e
vivienhennig <vivienhennig@web.de>
2020-10-01 11:01:15 +0200

    add day 8 project

---
+++ Day-8-Event-Keycodes/index.js
@@ -0,0 +1,34 @@
+const body = document.querySelector('body');
+const keyDisplayContainer = document.getElementById('keyCodeContainer');
+
+function getKeyCode(event) {
+
+    if (keyDisplayContainer.childElementCount != 0) {
+        keyDisplayContainer.removeChild(keyDisplayContainer.childNodes[2]);
+        keyDisplayContainer.removeChild(keyDisplayContainer.childNodes[1]);
+    };
+
+    const keyContainer = createDisplayDiv(event, 'event.key');
+    const keyCodeContainer = createDisplayDiv(event, 'event.keyCode');
+
+    keyDisplayContainer.appendChild(keyContainer);
+    keyDisplayContainer.appendChild(keyCodeContainer);
+};
+
+function createDisplayDiv(event, title) {
+    const container = document.createElement('div');
+    const titleParagraph = document.createElement('p');
+    const titleText = `${title}`;
+    const contentParagraph = document.createElement('p');
+
+    container.classList.add('displayKey');
+    titleParagraph.innerText = titleText;
+    contentParagraph.innerText = `${event.keyCode}`;
+
+    container.appendChild(titleParagraph);
+    container.appendChild(contentParagraph);
+
+    return container;
+}
+
+body.addEventListener('keypress', (event) => getKeyCode(event));
