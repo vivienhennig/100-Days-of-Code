6d3efde5e4dbac6ce7bc5c2b1c4fc9219ccab60d
vivienhennig <vivienhennig@web.de>
2020-10-05 12:06:04 +0200

    add day 12 project

---
+++ Day-12-Review-Page/index.js
@@ -0,0 +1,27 @@
+const stars = document.querySelectorAll('.star');
+
+function addHoverColorYellow(event) {
+    const rating = event.target.getAttribute('data-star');
+
+    for (let i = 0; i <= rating; i++) {
+        stars[i].classList.add('ratedStar');
+    };
+};
+
+function removeHoverColorYellow() {
+    stars.forEach(star => star.classList.remove('ratedStar'));
+};
+
+function addClickColorYellow(event) {
+    const rating = event.target.getAttribute('data-star');
+
+    for (let i = 0; i <= rating; i++) {
+        stars[i].id = `star${i}`;
+    };
+
+}
+
+
+stars.forEach(star => star.addEventListener('mouseover', (event) => addHoverColorYellow(event)));
+stars.forEach(star => star.addEventListener('mouseleave', () => removeHoverColorYellow()));
+stars.forEach(star => star.addEventListener('click', (event) => addClickColorYellow(event)));
