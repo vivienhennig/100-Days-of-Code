1f90e8dd2be99b2fdae4389baa50ea19d8b4d62f
vivienhennig <vivienhennig@web.de>
2020-10-23 20:19:53 +0200

    update day 29 project

--- Day-29-Corona-Dashboard/index.js
+++ Day-29-Corona-Dashboard/index.js
@@ -1,8 +1,8 @@
-const dailyTotalGlobalCard = document.getElementById('dailyTotalGlobalCard');
-const dailyTotalByCountry = document.getElementById('dailyTotalByCountry');
+const totalGlobal = document.getElementById('totalGlobalCard');
+const totalByCountry = document.getElementById('totalByCountryCard');
+const dailyNewGlobal = document.getElementById('dailyNewGlobalCard');
 
 const date = new Date();
-
 
 async function fetchAPI(urlSlug) {
     const url = `https://api.covid19api.com/${urlSlug}`;
@@ -22,7 +22,7 @@ function setSelect() {
 
     fetchAPI('countries')
         .then(data => {
-            countries = data;
+            data.sort((a, b) => a.Country > b.Country);
             data.forEach(country => {
                 const option = document.createElement('option');
                 option.innerText = country.Country;
@@ -33,45 +33,69 @@ function setSelect() {
         })
         .catch(error => console.error(error));
 
-    select.addEventListener('click', (event) => displayCountOfCountry(event))
-    dailyTotalByCountry.appendChild(select);
+    select.addEventListener('click', (event) => displayCountOfCountry(event));
+    totalByCountry.appendChild(select);
 };
 
 
 function displayCountOfCountry(event) {
 
-
-    console.log(dailyTotalByCountry.childElementCount);
-    console.log(event.target);
-
     if (event.target.tagName === 'OPTION') {
         const slug = event.target.getAttribute('data-slug');
-        console.log(slug);
-        console.log(date.toISOString());
-        console.log(date - 1);
-        fetchAPI(`total/country/${slug}/status/confirmed?from=${date.toISOString()}&to=${date.toISOString()}`)
+        fetchAPI(`total/country/${slug}/status/confirmed`)
             .then(data => {
                 if (data.length != 0) {
                     const numberDisplay = document.createElement('p');
                     const index = (data.length) - 1;
-                    numberDisplay.innerText = data[index].Cases;
-                    if (dailyTotalByCountry.childElementCount >= 3) {
-                        dailyTotalByCountry.removeChild(dailyTotalByCountry.childNodes[dailyTotalByCountry.childElementCount + 1]);
+                    const number = formatNumber(data[index].Cases);
+                    numberDisplay.innerText = number;
+                    if (totalByCountry.childElementCount >= 3) {
+                        totalByCountry.removeChild(totalByCountry.childNodes[totalByCountry.childElementCount + 1]);
                     };
-                    dailyTotalByCountry.appendChild(numberDisplay);
+                    totalByCountry.appendChild(numberDisplay);
                 } else {
                     const numberDisplay = document.createElement('p');
                     numberDisplay.innerText = `sorry, we have no data for ${event.target.innerText}`;
-                    if (dailyTotalByCountry.childElementCount >= 3) {
-                        dailyTotalByCountry.removeChild(dailyTotalByCountry.childNodes[dailyTotalByCountry.childElementCount + 1]);
+                    if (totalByCountry.childElementCount >= 3) {
+                        totalByCountry.removeChild(totalByCountry.childNodes[totalByCountry.childElementCount + 1]);
                     };
-                    dailyTotalByCountry.appendChild(numberDisplay);
+                    totalByCountry.appendChild(numberDisplay);
                 };
-                console.log(data.length);
             })
             .catch(error => console.error(error));
     };
 };
 
 
+function displayCountGlobal() {
+    fetchAPI('summary')
+        .then(data => {
+            const numberDisplay = document.createElement('p');
+            const number = formatNumber(data.Global.TotalConfirmed);
+            numberDisplay.innerText = number;
+            totalGlobal.appendChild(numberDisplay);
+            console.log(data);
+        })
+        .catch(error => console.error(error));
+};
+
+
+function displayDailyNewCountGlobal() {
+    fetchAPI('summary')
+        .then(data => {
+            const numberDisplay = document.createElement('p');
+            const number = formatNumber(data.Global.NewConfirmed);
+            numberDisplay.innerText = number;
+            dailyNewGlobal.appendChild(numberDisplay);
+        })
+        .catch(error => console.error(error));
+};
+
+
+function formatNumber(num) {
+    return num.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, '$1,')
+}
+
+displayDailyNewCountGlobal();
+displayCountGlobal();
 setSelect();
